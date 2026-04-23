75389e7113be00ccf9c8eb8d5052e0aaa0b8bf64
Abhishek271-eng <abhishekdevanagaon2712005@gmail.com>
2026-04-23 17:13:01 +0530

    Delete backend/app.py

--- backend/app.py
+++
@@ -1,7 +0,0 @@
-from backend.app import app
-import os
-
-if __name__ == '__main__':
-    port = int(os.getenv('PORT', '5000'))
-    debug = os.getenv('FLASK_DEBUG') == '1'
-    app.run(host='0.0.0.0', debug=debug, port=port)
